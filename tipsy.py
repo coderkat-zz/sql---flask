fe9a5711d0a51a73936bab43987811fafd385970
Hackbright Student <user@mini-004.(none)>
2013-03-26 13:02:09 -0700

    cleaned up DB calls, URLs with url_for, task created by logged in user, added nav bar

--- tipsy.py
+++ tipsy.py
@@ -1,54 +1,61 @@
-from flask import Flask, flash, session, render_template, request, redirect, url_for
-import model
-import urllib
-import re
+from flask import Flask, flash, session, render_template, request, redirect, url_for, g
+import model # our other Python file that has the SQL query functions
+import urllib # used for URL encoding
+import re # to do regex date validation
 
 app = Flask(__name__) # make a new instance of the Flask class
 app.secret_key = 'some_secret'
 
-current_user = None
+# function that we can call before each view is executed
+@app.before_request
+def set_up_db():
+	g.db = model.connect_db() # global variable, db
+
+# function to close db connection after each view is rendered
+@app.teardown_request
+def close_db(e): # e is to allow this function to still be called even if some error happens
+	g.db.close()
 
 # a decorated function aka a 'view'
 @app.route("/") # this is the decorate that tells Flask what URL/route is attached to this index function, i.e. maps URL to function
 def index():
-	if 'username' in session and 'password' in session:
-		db = model.connect_db()
-		authenticate_result = model.authenticate(db, session['username'], session['password'])
-		if authenticate_result:
-			current_user = session['username']
-		else:
-			flash("Incorrect username or password")
-			return redirect("/login")
+	return render_template("index.html") # the return value that's sent back to the browser as read from our index html file
 
-	elif ('username','password') in session:
-		flash("You need to enter both a username and a password")
-		return redirect("/login")
-
-	else:
-		current_user = "Not signed in"
-
-	return render_template("index.html", user_name=current_user) # the return value that's sent back to the browser as read from our index html file
-
-@app.route('/login', methods=['GET', 'POST'])
+# login page
+@app.route('/login')
 def login():
-	if request.method == 'POST':
-		session['username'] = request.form['username']
-		session['password'] = request.form['password']
-		return redirect("/")
 	return render_template("login.html")
+
+# verifying login credentials
+@app.route('/authenticate', methods=["POST"])
+def authenticate():
+	email = request.form['username']
+	password = request.form['password']
+
+	user_info = model.authenticate(g.db, email, password)
+
+	# if credentials are wrong or incomplete
+	if not user_info:
+		flash("Incorrect username or password")
+		return redirect(url_for("login"))
+
+	else: # adding user info into session dictionary to pull out and use later
+		session['user_id'] = user_info['id']
+		session['username'] = user_info['email']
+		return redirect(url_for("index"))
 
 @app.route('/logout')
 def logout():
 	# remove existing username from session
 	session.pop('username', None)
-	session.pop('password', None)
-	return redirect("/")
+	session.pop('user_id', None)
+	flash("Successfully logged out!")
+	return redirect(url_for('index'))
 
 @app.route("/tasks")
 def list_tasks():
 	#TODO: Make another thing that returns everything nessecary for this view, by using a join instead of having python do multiple queries.
-	db = model.connect_db()
-	tasks_from_db = model.get_tasks(db, None) # this returns a list of dictionaries
+	tasks_from_db = model.get_tasks(g.db, None) # this returns a list of dictionaries
 
 	for dicts in tasks_from_db:
 		for key in ("title", "description"):
@@ -60,7 +67,7 @@ def list_tasks():
 def new_tasks():
 	if 'username' not in session:
 		flash("You must be logged in to create a new task for yourself.")
-		return redirect("/")
+		return redirect(url_for("index"))
 
 	else:
 		return render_template("new_task.html")
@@ -73,7 +80,7 @@ def save_task():
 	# check for title in task form: if none, flash error & redirect
 	if not task_title:
 		flash("You must enter a title for your task")
-		return redirect("/new_task")
+		return redirect(url_for('new_tasks'))
 	
 	task_description = urllib.quote(request.form['task_description'])
 	
@@ -83,14 +90,13 @@ def save_task():
 	date_comparison = re.match(r'^((0[0-9])|(1[0-2]))-((0[1-9])|(1|2)[0-9]|(3[0|1]))-((1[3-9])|([2-9][0-9]))', task_due_date)
 	
 	if date_comparison:
-		db = model.connect_db()
-			# Assume that all tasks are attached to user 1
-		task_id = model.new_task(db, task_title, task_description, task_due_date, 1)
-		return redirect("/tasks")
+		# Assume that all tasks are attached to user 1
+		task_id = model.new_task(g.db, task_title, task_description, task_due_date, session['user_id'])
+		return redirect(url_for('list_tasks'))
 
 	else:
 		flash("Your date is not in the right format!") #flashing system basically makes it possible to record a message at the end of a request and access it next request and only next request
-		return redirect("/new_task")
+		return redirect(url_for('new_tasks'))
 
 
 if __name__ == "__main__": # start web app server when we run program from command line
